548bbbc319ea5e331dec3f1b9fdece4715d15322
Shucheng Yang <kelvin1020@foxmail.com>
2020-10-10 11:25:08 +0800

    re find sender&email address

--- Chapter10/listing10-10.py
+++ Chapter10/listing10-10.py
@@ -1,6 +1,18 @@
 # find_sender.py
 import fileinput, re
+
+#find sender
 pat = re.compile('From: (.*) <.*?>$')
 for line in fileinput.input():
     m = pat.match(line)
-    if m: print m.group(1)
+    if m: print(m.group(1))
+        
+#find email
+pat = re.compile(r'[a-z\-\.]+@[a-z\-\.]+', re.IGNORECASE)
+addresses = set()
+for line in fileinput.input():
+    for address in pat.findall(line):
+        addresses.add(address)
+
+for address in sorted(addresses):
+    print(address)
